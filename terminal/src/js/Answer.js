1192cd58cd0c35356102cb9b0d64077cbadb94e0
Yauhen Manuila <manuila.yauhen@gmail.com>
2018-03-27 00:14:39 +0300

    Update Answer.js

--- terminal/src/js/Answer.js
+++ terminal/src/js/Answer.js
@@ -1,12 +1,7 @@
-const uuidv1 = require('uuid/v1');
 class Answer {
-    constructor(id = uuidv1(), title, description) {
-        this.id = id;
+    constructor(title, description) {
         this.title = title;
         this.description = description;
-      }
-      get getId() {
-        return this.id;
       }
       get getTitle() {
         return this.title;
