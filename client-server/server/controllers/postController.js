d95243a70095f73bab8eb851a21da17381e09b34
Yauhen Manuila <manuila.yauhen@gmail.com>
2018-05-17 20:00:08 +0300

    update PostController

--- client-server/server/controllers/postController.js
+++ client-server/server/controllers/postController.js
@@ -4,39 +4,43 @@ import MongoDBPostDAO from '../dao/MongoDBPostDAO';
 const postDAO = new MongoDBPostDAO();
 const postService = new PostService(postDAO);
 
-export function getPosts(req, res){
-  postService.getAll()
-    .then(posts => res.status(200).json({ posts }))
-    .catch(error => res.status(404).send(error));
-}
+class PostController {
 
-export function deletePost(req, res) {
-  const idPost = req.params.id;
-  postService.remove(idPost)
-    .then(post => res.status(200).json({ post }))
-    .catch(error => res.status(400).send(error));
+  getPosts(req, res) {
+    postService.getAll()
+      .then(posts => res.status(200).json({ posts }))
+      .catch(error => res.status(404).send(error));
+  }
+  
+  deletePost(req, res) {
+    const idPost = req.params.id;
+    postService.remove(idPost)
+      .then(post => res.status(200).json({ post }))
+      .catch(error => res.status(400).send(error));
+  }
+  
+  addPost(req, res) {
+    if (!req.body.post.title) res.status(403).end();
+    const newPost = {
+      title: req.body.post.title,
+      description: req.body.post.description,
+    };
+    postService.add(newPost)
+      .then(post => res.status(200).json({ post }))
+      .catch(error => res.status(400).send(error));
+  }
+  
+  updatePost(req, res) {
+    postService.update(req.params.id, req.body.post)
+      .then(post => res.status(200).json({ post }))
+      .catch(error => res.status(400).send(error));
+  }
+  
+  getPost(req, res) {
+    const idPost = req.params.id;
+    postService.getById(idPost)
+      .then(post => res.status(200).json({ post }))
+      .catch(error => res.status(404).send(error));
+  }
 }
-
-export function addPost(req, res) {
-  if (!req.body.post.title) res.status(403).end();
-  const newPost = {
-    title: req.body.post.title,
-    description: req.body.post.description,
-  };
-  postService.add(newPost)
-    .then(post => res.status(200).json({ post }))
-    .catch(error => res.status(400).send(error));
-}
-
-export function updatePost(req, res) {
-  postService.update(req.params.id, req.body.post)
-    .then(post => res.status(200).json({ post }))
-    .catch(error => res.status(400).send(error));
-}
-
-export function getPost(req, res) {
-  const idPost = req.params.id;
-  postService.getById(idPost)
-    .then(post => res.status(200).json({ post }))
-    .catch(error => res.status(404).send(error));
-}
+export default PostController;
--- client-server/server/routes/postRoutes.js
+++ client-server/server/routes/postRoutes.js
@@ -1,20 +1,20 @@
 import { Router } from 'express';
-import * as PostController from '../controllers/postController';
+import PostController from '../controllers/PostController';
 const router = new Router();
-
+const postController = new PostController();
 // Get all Posts
-router.route('/').get(PostController.getPosts);
+router.route('/').get(postController.getPosts);
 
 // Delete a post by id
-router.route('/:id').delete(PostController.deletePost);
+router.route('/:id').delete(postController.deletePost);
 
 // Add a new Post
-router.route('/').post(PostController.addPost);
+router.route('/').post(postController.addPost);
 
 // Get one post by id
-router.route('/:id').get(PostController.getPost);
+router.route('/:id').get(postController.getPost);
 
 // Update post by id
-router.route('/:id').put(PostController.updatePost);
+router.route('/:id').put(postController.updatePost);
 
 export default router;
